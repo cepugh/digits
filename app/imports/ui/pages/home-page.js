f64d20da09b899ec783dd3f6e25f2ad4c8cf1984
Chaselyn Pugh <chasepugh@dhcp-168-105-8-40.frear.manoa.hawaii.edu>
2017-03-15 10:05:44 -1000

    Sort by column
    Added tablesort.js page to the pages directory and created an events
    function to call $(‘table').tablesort(); when the table in the homepage
    is clicked.

--- app/imports/ui/pages/home-page.js
+++ app/imports/ui/pages/home-page.js
@@ -1,14 +1,17 @@
 import { Template } from 'meteor/templating';
 import { Contacts } from '../../api/contacts/contacts.js';
+import './tablesort.js';
 
 Template.Home_Page.helpers({
-
-  /**
-   * @returns {*} All of the Contacts documents.
-   */
   contactsList() {
     return Contacts.find();
   },
+});
+
+Template.Home_Page.events({
+    'click .sortable-table'() {
+     $('table').tablesort();
+},
 });
 
 Template.Home_Page.onCreated(function onCreated() {
